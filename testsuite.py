ea96db69538a68054203975b4863718afa891636
Tom Schouten <tom@zwizwa.be>
2014-02-28 13:15:19 +0100

    tools.py

--- testsuite.py
+++ testsuite.py
@@ -1,44 +1,20 @@
 import sys
 if sys.version_info[0] != 3:
     raise NameError("Python version 3 needed.")
+sys.path.append(".")
 sys.path.append("build")
 import pylacore
+from tools import *
 
 # Core module doesn't have any functionality.  Should this be removed?
 core = pylacore.pylacore()
 # print(core.identify())
 
-
-def oversample(lst,n):
-    for e in lst:
-        for i in range(n):
-            yield(e)
-
-# Bit generators
-# Note that SPI is MSB first while UART is LSB first.
-def number_bits(number, nb):
-    for i in range(nb):
-        yield (number >> i) & 1
-
-def bytes_bits(bs):
-    for c in bs:
-        for b in number_bits(c, 8):
-            yield b
-
-def parity(bits):
-    p = 0
-    for b in bits:
-        p ^= b
-    return p
-
-def uart_frame(byte):
-    l = list(number_bits(byte, 8))
-    return [0] + l + [parity(l)] + [1]
-
 def check(cond, msg):
     if not cond:
         raise NameError(msg)
     print("PASS: %s" % msg)
+
 
 # UART
 def test_uart():
---
+++ tools.py
@@ -0,0 +1,27 @@
+
+# Bit generators
+# Note that SPI is MSB first while UART is LSB first.
+def number_bits(number, nb):
+    for i in range(nb):
+        yield (number >> i) & 1
+
+def bytes_bits(bs):
+    for c in bs:
+        for b in number_bits(c, 8):
+            yield b
+
+def oversample(lst,n):
+    for e in lst:
+        for i in range(n):
+            yield(e)
+
+def parity(bits):
+    p = 0
+    for b in bits:
+        p ^= b
+    return p
+
+def uart_frame(byte):
+    l = list(number_bits(byte, 8))
+    return [0] + l + [parity(l)] + [1]
+
