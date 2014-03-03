021f3c7726f2babb5a1b54cda2f41601eb4bce07
Tom Schouten <tom@zwizwa.be>
2014-03-03 17:01:25 +0100

    cleanup examples.py

--- examples.py
+++ examples.py
@@ -3,26 +3,28 @@ from stream import *
 
 # DUMP SPI as ASCII
 
-def dump_spi_hex():
-    """SPI HEX data logger."""
-    print_hex(
-        saleae_with(
-            syncser(),
-            config={
-                'clock_channel'  : 0,
-                'data_channel'   : 1,
-                'frame_channel'  : 2,
-                'frame_active'   : 0,  # SPI /CS  
-                'clock_edge'     : 1,  # sampling edge: 0->1
-                'clock_polarity' : 0,  # clock idle polarity
-            }))
+def saleae_spi():
+    return saleae_with(
+        syncser(),
+        config={
+            'clock_channel'  : 0,
+            'data_channel'   : 1,
+            'frame_channel'  : 2,
+            'frame_active'   : 0,  # SPI /CS  
+            'clock_edge'     : 1,  # sampling edge: 0->1
+            'clock_polarity' : 0,  # clock idle polarity
+        })
 
-dump_spi_hex()
+def saleae_uart():
+    return saleae_with(
+        uart(),
+        config={
+            'channel'  : 3,
+            'baudrate' : 115200,
+        })
+    
 
-# test_saleae_syncser()
-# dump_diff()
-
-# dump_syncser(frame=2,dump=print_hex)
-
+# print_hex(saleae_spi())
+print_ascii(saleae_uart())
 
 
