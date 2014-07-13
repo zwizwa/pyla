7b35e63605e78b28748fc5615f7b004a1467be4e
Tom Schouten <tom@zwizwa.be>
2014-07-13 13:35:25 -0400

    midi parser

---
+++ example_midi.py
@@ -0,0 +1,19 @@
+from pyla import *
+from stream import *
+
+
+## Currently samplerate can only be set once at startup.
+# pyla.saleae.start(24000000)
+pyla.saleae.start(4000000)
+
+def saleae_uart():
+    return saleae_with(
+        uart().config({
+            'channel'  : 3,
+            'baudrate' : 31250,
+        }))
+
+# print_ascii(saleae_uart())
+print_midi(saleae_uart())
+
+
